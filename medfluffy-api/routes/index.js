35097fffab4682902c1c51b3d78ad4954a075d85
dina <89837465+rhmdin@users.noreply.github.com>
2023-06-01 16:58:21 +0700

    configuring db

--- medfluffy-api/routes/index.js
+++ medfluffy-api/routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+require('dotenv').config();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const title = 'MedFluffy API';
-  const description = 'Welcome to documentation page for MedFluffy API';
+  const description = 'Welcome to documentation page for MedFluffy API with database: '+ process.env.DB_NAME;
   res.render("index", { title, description});
 });
 
