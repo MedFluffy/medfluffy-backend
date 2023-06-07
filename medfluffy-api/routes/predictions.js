9ca0eeb56b873b7b53e03c3086694357224e63ad
dina <89837465+rhmdin@users.noreply.github.com>
2023-06-08 00:55:26 +0700

    prediction add

--- medfluffy-api/routes/predictions.js
+++ medfluffy-api/routes/predictions.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const validator = require('fastest-validator');
-const v = new validator();
 var controller = require('../controllers/Predictions');
 /* GET home page. */
 router.get('/', async (req, res) => {
     res.json({message: "ini halaman detail prediction"})
 });
-router.post('/add', async (req, res) => {
-    res.json({message: "ini endpoint tambah foto"});
-});
+router.post('/add', controller.add);
 router.get('/show', controller.showAll);
 router.get('/show/:id', controller.showOne);
 
